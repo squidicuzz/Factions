82a35f6937b0ac8b35d42c2fbaafff621ecf4094
Squidicuz <jono324@gmail.com>
2013-04-15 23:37:59 -0400

    Corrected issue with players joining a faction as the wrong rank.

--- src/com/massivecraft/factions/cmd/CmdJoin.java
+++ src/com/massivecraft/factions/cmd/CmdJoin.java
@@ -91,9 +91,6 @@ public class CmdJoin extends FCommand
 		// then make 'em pay (if applicable)
         if (samePlayer && ! payForCommand(Conf.econCostJoin, "to join a faction", "for joining a faction")) return;
 
-        Rel role = Conf.factionRankDefault;
-        fme.setRole(role); // They have just joined a faction, start them out on the lowest rank (default config).
-
         if (!samePlayer)
             fplayer.msg("<i>%s moved you into the faction %s.", fme.describeTo(fplayer, true), faction.getTag(fplayer));
         faction.msg("<i>%s joined your faction.", fplayer.describeTo(faction, true));
@@ -101,6 +98,7 @@ public class CmdJoin extends FCommand
 
 		fplayer.resetFactionData();
 		fplayer.setFaction(faction);
+		fplayer.setRole(Conf.factionRankDefault); // They have just joined a faction, start them out on the lowest rank (default config).
 
         faction.deinvite(fplayer);
 		faction.deinvite(fplayer);
